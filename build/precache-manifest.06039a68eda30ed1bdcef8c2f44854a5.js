0f1b511ff6cde79e00b0d4c838816197bbbed1d0
santong <bensendbs@gmail.com>
2019-05-21 19:34:57 +0800

    dev

---
+++ build/precache-manifest.06039a68eda30ed1bdcef8c2f44854a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be2deb34537f34006e8f496a3c06f23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80a75303312f9bbd39d",
+    "url": "/static/css/main.54981800.chunk.css"
+  },
+  {
+    "revision": "0f44f52868a50c2f6362",
+    "url": "/static/js/2.d23e1248.chunk.js"
+  },
+  {
+    "revision": "f80a75303312f9bbd39d",
+    "url": "/static/js/main.ac6fbe08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
