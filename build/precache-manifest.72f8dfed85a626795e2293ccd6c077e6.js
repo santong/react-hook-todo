3f800e079afbea47d893cb77e5e9bae9c10fe3d9
santong <bensendbs@gmail.com>
2019-05-22 17:42:15 +0800

    test

---
+++ build/precache-manifest.72f8dfed85a626795e2293ccd6c077e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb9307cc01c71bdb5e305f0d6cb415e",
+    "url": "//santongdbs.com/react-hook-todo/build/index.html"
+  },
+  {
+    "revision": "f80a75303312f9bbd39d",
+    "url": "//santongdbs.com/react-hook-todo/build/static/css/main.54981800.chunk.css"
+  },
+  {
+    "revision": "0f44f52868a50c2f6362",
+    "url": "//santongdbs.com/react-hook-todo/build/static/js/2.d23e1248.chunk.js"
+  },
+  {
+    "revision": "f80a75303312f9bbd39d",
+    "url": "//santongdbs.com/react-hook-todo/build/static/js/main.ac6fbe08.chunk.js"
+  },
+  {
+    "revision": "0aebe6d9d755b1a41dc1",
+    "url": "//santongdbs.com/react-hook-todo/build/static/js/runtime~main.d2b85733.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06039a68eda30ed1bdcef8c2f44854a5.js"
+  "//santongdbs.com/react-hook-todo/build/precache-manifest.72f8dfed85a626795e2293ccd6c077e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//santongdbs.com/react-hook-todo/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
